98cead75b15c0b1d0f8f555147d69e6911655a57
ValCordDev <88210628+ValCordDev@users.noreply.github.com>
2024-11-12 14:18:58 +0100

    Update page.tsx

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,17 +5,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={`${inter.className} w-screen flex justify-center`}>
-      <nav className="bg-white/10 backdrop-blur-md navbar">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Dev Raj</a>
+    <div className={`${inter.className} w-screen flex justify-center bg-white min-h-screen`}>
+      <div className="bg-[#232323] w-full m-2 rounded-3xl">
+        <nav className="backdrop-blur-md w-full navbar rounded-t-2xl">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl font-bold">Dev Raj</a>
+          </div>
+          <div className="flex-none gap-2">
+            <a className="btn btn-ghost">Web</a>
+            <a className="btn btn-ghost">Utvikling</a>
+            <a className="btn btn-ghost">Grafikk</a>
+            <a className="btn btn-ghost">Medie</a>
+            <a className="btn btn-ghost">Kontakt</a>
+          </div>
+        </nav>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          
-        </button>
-      </div>
-      </nav>
       <main>
         
       </main>
